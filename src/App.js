1de6157824e7238b305bee3f220105bd2e3c8e4c
vaishnavi pratale <pratalev@gmail.com>
2024-06-26 15:10:10 +0530

    Update App.js with new changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { Route, Routes } from 'react-router-dom';
+import SignUp from './Component/SignUp';
+import LogIn from './Component/LogIn';
+import Welcome from './Component/pagese/Welcome';
+import Header from './Component/Header/Header';
+import CompleteProfile from './Component/pagese/CompleteProfile';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Header/>
+    <Routes>
+      <Route path='/' element={<SignUp/>} />
+      <Route path="login" element={<LogIn />} />
+      <Route path='/welcome' element={<Welcome/>}/>
+      <Route path='welcome/completeprofile' element={<CompleteProfile/>} />
+    </Routes>
+    </>
   );
 }
 
